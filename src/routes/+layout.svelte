79fabaf3b32472698e57fc65c5d28f0f8aa0f091
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-05-31 22:45:58 +0700

    build layout svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import './layout.css';
+	import Navbar from '$lib/components/Navbar.svelte';
 	import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+	let { children, data } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Navbar user={data?.user ?? null} />
+
 {@render children()}
